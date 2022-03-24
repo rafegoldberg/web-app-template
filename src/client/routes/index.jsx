e255e89b77d3aaf3dd7f9f0961c5a9697ab53828
Rafe Goldberg <rafegoldberg@gmail.com>
2022-03-24 06:54:39 -0700

    add user routes; nix deprecated pages components

--- src/client/routes/index.jsx
+++ src/client/routes/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
+import SignUp from "./Users";
+
 const AppRouter = () => (
   <Routes>
     <Route path="/">
-      <Route index element={<b>👋 Hey World!</b>} />
+      <Route path="signup" element={<SignUp />} />
+      <Route index element={<b>👋 Hi world!</b>} />
       <Route path="*" element={<b>⛔️ Not Found.</b>} />
     </Route>
   </Routes>
---
+++ src/client/ui/HTMLForm/index.jsx
@@ -0,0 +1,35 @@
+import React, { useRef } from "react";
+
+const { SERVER_URI } = process.env;
+
+const HTMLForm = ({ children, action, method = "GET" }) => {
+  const $form = useRef(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { current: el } = $form;
+    const data = Object.fromEntries(new FormData(el));
+    const path = `${SERVER_URI}/${action}`;
+    const res = await fetch(path, {
+      method,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    const { error, ...json } = await res.json();
+    if (error) console.error(error);
+    else console.log(json);
+  };
+
+  return (
+    <form
+      ref={$form}
+      onSubmit={handleSubmit}
+      action={action}
+      method={method}
+      children={children}
+    />
+  );
+};
+
+export default HTMLForm;
