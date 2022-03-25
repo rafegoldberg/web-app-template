2391ec4e61242c15594ef7521baa5937b788caf2
Rafe Goldberg <rafegoldberg@gmail.com>
2022-03-25 08:37:08 -0700

    update log level config/env var

--- main.mjs
+++ main.mjs
@@ -6,7 +6,7 @@ import app from "./src/server/app.mjs";
 const { PORT } = process.env;
 
 app.listen(PORT, () => {
-  if (config.logs) {
+  if (config.debug) {
     console.log();
     console.log(`┌[SERVER STARTED]`);
     console.log(`└>Listening on: http://localhost:${PORT}`);
--- src/db/connect.mjs
+++ src/db/connect.mjs
@@ -3,7 +3,7 @@ import m from "mongoose";
 
 const { NODE_ENV: env = "development" } = process.env;
 
-if (config.logs) {
+if (config.debug) {
   m.connection.on("connected", () => console.log("DB Connected"));
   m.connection.on("disconnected", () => console.log("DB Disconnected"));
   m.connection.on("error", (err) => console.error(err));
@@ -12,7 +12,7 @@ if (config.logs) {
 export const disconnect = m.disconnect;
 
 export const connect = () => {
-  m.set("debug", config.mongo.debug);
+  m.set("debug", config.debug || config.mongo.debug);
   return m.connect(config.mongo.uri, {
     autoIndex: true,
     autoCreate: true,
