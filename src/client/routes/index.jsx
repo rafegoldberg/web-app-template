135fb7e2df2e092c539e1ac3772ff6d8c1bd6265
Rafe Goldberg <rafegoldberg@gmail.com>
2022-03-25 08:45:48 -0700

    working sign in/out/up views with redirection!

--- src/client/routes/index.jsx
+++ src/client/routes/index.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Outlet } from "react-router-dom";
 
-import SignUp from "./Users";
+import { SignIn, SignUp } from "./Auth";
+
+const App = () => (
+  <main>
+    <h3>Web App Scaffold</h3>
+    <Outlet />
+  </main>
+);
 
 const AppRouter = () => (
   <Routes>
-    <Route path="/">
-      <Route path="signup" element={<SignUp />} />
+    <Route path="/" element={<App />}>
+      <Route path="sign">
+        <Route path="up" element={<SignUp />} />
+        <Route path="in" element={<SignIn />} />
+      </Route>
       <Route index element={<b>👋 Hi world!</b>} />
       <Route path="*" element={<b>⛔️ Not Found.</b>} />
     </Route>
